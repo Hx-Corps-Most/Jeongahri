d962d4b4ee19dbf49ad6a0e25668c4a6e59ad655
Jeongahri <jeongah.h.shin@gmail.com>
2017-09-19 14:45:12 +0900

    search result parsing

---
+++ HtmlParsingApp/HtmlParsingApp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HtmlParsingApp
+{
+	public partial class MainPage : ContentPage
+	{
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        void OnParsingBtnClick(object sender, EventHandler e)
+        {
+            String relatives = "";
+            DisplayAlert("Wait", "Parsing Activity has been started.", "OK");
+
+            targetLabel.BindingContext = relatives;
+
+            HtmlAgilityPack.HtmlDocument myDoc = new HtmlAgilityPack.HtmlDocument();
+            myDoc.LoadHtml("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%A0%95%EC%95%84");
+
+
+            foreach (HtmlNode parentNode1 in myDoc.DocumentNode.SelectNodes("//dd[@class='1st_relate']"))
+            {
+               
+                foreach (HtmlNode childNode1 in parentNode1.SelectNodes(".//a"))
+                {
+                    //string attributeValue = childNode.GetAttributeValue("href", "");
+                    relatives += childNode1.InnerText + "\n";
+                }
+            }
+
+            foreach (HtmlNode parentNode2 in myDoc.DocumentNode.SelectNodes("//div[@class='realtime_srch']"))
+            {
+                foreach (HtmlNode childNode2 in parentNode2.SelectNodes(".//span[@class='tit']"))
+                {
+                    //string attributeValue = childNode.GetAttributeValue("href", "");
+                    relatives += childNode2.InnerText + "\n";
+                }
+            }
+
+            //MainPageViewModel mainPageViewModel = new MainPageViewModel();
+            //this.BindingContext = mainPageViewModel;
+        }
+	}
+}
---
+++ HtmlParsingApp/HtmlParsingApp/MainPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlParsingApp
+{
+    public class MainPageViewModel
+    {
+        public string parsedText;
+
+
+
+    }
+}
